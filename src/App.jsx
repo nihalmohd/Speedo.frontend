f93ccb6f58a00a689b42c60bd9ec769d150c2203
nihalmohd <nihalmohd828@gmail.com>
2024-10-02 14:42:46 +0530

    Basic setup is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
+import { BrowserRouter as Router ,Route,Routes } from 'react-router-dom'
 import './App.css'
+import Login from './assets/Components/pages/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+     <Router>
+      <Routes>
+        <Route path='/*' element={<Login/>}></Route>
+      </Routes>
+     </Router>
   )
 }
 
